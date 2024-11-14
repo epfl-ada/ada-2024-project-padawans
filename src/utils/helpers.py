532d710ba77d50659982035f195c2c0f14d64d93
clarachappuis <cla28@bluewin.ch>
2024-11-14 21:38:38 +0100

    reorganization of code, add box_office_extraction.py file

--- src/utils/helpers.py
+++ src/utils/helpers.py
@@ -21,6 +21,7 @@ def convert_to_list(df, column_name):
     df[column_name] = df[column_name].apply(lambda x: list(x.values()) if isinstance(x, dict) else None)
     return df[column_name]
 
+
 def extract_year(value):
     # Check if the value is a year (4-digit number) and return it as is
     if isinstance(value, int) or (isinstance(value, str) and value.isdigit() and len(value) == 4):
@@ -31,10 +32,12 @@ def extract_year(value):
     except (ValueError, TypeError):
         return np.nan  # Return NaN if conversion fails
     
+    
 def clean_ids(s, pattern=' '): #remove in string a pattern
     s = str(s).replace(pattern, '')
     return s
     
+
 def get_box_office(imdb_ID): #return the box-office revenue for a specific imdb_id
     ia = IMDb()
     movie = ia.get_movie(imdb_ID)
@@ -53,11 +56,9 @@ def remove_named_entities(text):
         return ' '.join([token.text for token in doc if token.ent_type_ != "PERSON"])  # Exclude PERSON entities
 
 
-
 def synopses_processing(sentences):
+    # Process a list of sentences by removing named entities, filtering out stop words and converting words to lowercase
 
-    # ADD DOCSTRING
-    
     stop_words = set(stopwords.words('english'))
     
     # Remove named entities
